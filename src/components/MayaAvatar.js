219f881ac4902f5d96974fd490b478bb1b098b55
camayankagarwal <mayank98270@gmail.com>
2025-04-23 13:10:00 +0530

    Add files via upload

---
+++ src/components/MayaAvatar.js
@@ -0,0 +1,9 @@
+
+export default function Avatar() {
+  return (
+    <div className="text-center">
+      <img src="https://via.placeholder.com/120" alt="Maya Avatar" className="mx-auto rounded-full w-24 h-24" />
+      <p className="mt-2 font-medium">Maya</p>
+    </div>
+  );
+}
